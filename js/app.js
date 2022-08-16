6f00dc997e735faabb631d2e76fc0708f92ce8a8
Mamdouh <mamdouh.mohammed919@gmail.com>
2022-08-16 16:52:41 +0200

    modified

--- js/app.js
+++ js/app.js
@@ -5,14 +5,15 @@ $(document).ready(function () {
         checkNames = $('#checkNames'),
         starterr = $('#start-err'),
         uploaderr = $('#upload-err'),
+        shuffleTime = $('#shuffle-time'),
+        timeerr = $('#time-err'),
         doneerr = $('#done-err'),
         fileInput = $('#file-input'),
         title = $('#title'),
         popSet = $('#pop-set'),
         numWin = $('#num-win'),
         AwardsBg = $('#awards-bg'),
-        container = $('#container'),
-        time = 5000,
+        time,
         i = 0,
         started = '',
         randomItem,
@@ -42,7 +43,7 @@ $(document).ready(function () {
         awardsArray = [];
 
 
-
+    //btn settings to start
     $('#done').on('click', function () {
         if (fileInput.val() == '') {
             doneerr.css('display', 'block')
@@ -52,7 +53,13 @@ $(document).ready(function () {
             doneerr.css('display', 'block')
             doneerr.text('يجب اختيار عدد الفائزين')
         }
+        else if (shuffleTime.val() == '' || shuffleTime.val() > 60) {
+            // console.log('hi');
+            doneerr.css('display', 'block')
+            doneerr.text('يجب وضع وقت صحيح')
+        }
         else {
+            doneerr.css('display', 'none')
             popSet.css('display', 'none')
             maxNumWinners = parseInt(numWin.val())
             let awardsInput = $('#awards-bg input');
@@ -62,11 +69,17 @@ $(document).ready(function () {
         }
     });
 
+    // icon setting open to edit
     $('#setting-icon').on('click', function () {
-        popSet.css('display', 'flex')
-        awardsArray = []
+        if (started == '') {
+            popSet.css('display', 'flex')
+            awardsArray = []
+        } else {
+            return false;
+        }
     });
 
+    // btn to add more winner
     $('#plus').on('click', function () {
         let val = parseInt(numWin.val())
         doneerr.css('display', 'none')
@@ -76,6 +89,7 @@ $(document).ready(function () {
         }
     });
 
+    //btn to remove winner
     $('#minus').on('click', function () {
         let val = parseInt(numWin.val())
         if (val != 0) {
@@ -83,6 +97,29 @@ $(document).ready(function () {
             AwardsBg.children("input:last").remove();
         }
     });
+
+    //make time to shuffle
+    shuffleTime.on('input', function () {
+        $(this).val($(this).val().replace(/[^-.0-9٠-٩]/g, ''));
+    });
+    shuffleTime.focusin('click', function () {
+        $(this).css({
+            "border": "3px solid #4245a8",
+            "color": "#4245a8"
+        });
+        timeerr.css('display', 'none')
+    });
+    shuffleTime.focusout(function (e) {
+        if ($(this).val() > 60) {
+            $(this).css({
+                "border": "3px solid #ff1c1c",
+                "color": "#ff1c1c"
+            });
+            timeerr.css('display', 'block')
+            timeerr.text('يجب اختيار وقت من اقل من 60')
+        }
+    });
+
 
 
     //start comp
@@ -98,11 +135,16 @@ $(document).ready(function () {
                 title.text(' اسم ' + myArray[numWinners])
                 started = 'done';
                 $('#start').fadeOut();
+                $('#time-countdown').fadeIn();
+                $('#time-countdown').css('display', 'flex')
+                time = shuffleTime.val() * 1000;
                 startTime();
+                countdownTimer();
             }
         }
     });
 
+    //time for take one winner
     function startTime() {
         let names = $('#names .information[data-id = 0]');
         timeoutExample = setTimeout(function () {
@@ -112,8 +154,11 @@ $(document).ready(function () {
             else {
                 for (let x = 0; x < names.length; x++) {
                     names[x].style.display = "none";
+                    // $(names[x]).fadeOut();
                 }
+                $(names[i]).fadeIn(40);
                 names[i].style.display = "flex";
+                // names[i].style.color = "#000";
                 i++
                 if (i == names.length) {
                     i = 0;
@@ -127,6 +172,7 @@ $(document).ready(function () {
                         names[x].style.display = "none";
                     }
                     randomItem = names[Math.floor(Math.random() * names.length)];
+                    $(randomItem).fadeIn()
                     randomItem.style.display = "flex";
                     showWinner();
                 }
@@ -135,13 +181,31 @@ $(document).ready(function () {
         }, 50);
     }
 
+    //countdown timer view
+    function countdownTimer() {
+        let countDownTimer = setInterval(setcountDownTimer, 1000);
+        let seconds = shuffleTime.val() - 1;
+        function setcountDownTimer() {
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            $('#time-countdown').text(seconds);
+            seconds--;
+            if (seconds < 0) {
+                stopcountDownTimer();
+            }
+        }
+        function stopcountDownTimer() {
+            clearInterval(countDownTimer);
+        }
+    }
+
+    //to show winner 
     function showWinner() {
-        // let again;
         timeoutExample = setTimeout(function () {
             createWinnerContent()
             if (numWinners == maxNumWinners - 1) {
                 namesCont.fadeOut();
                 $('#again').fadeOut();
+                $('#time-countdown').fadeOut();
                 title.text('انتهي السحب')
             }
             else {
@@ -149,18 +213,20 @@ $(document).ready(function () {
                 $('#again').append(' <i style="-webkit-transform: scaleX(-1);transform: scaleX(-1);" class="fa-solid fa-caret-right"></i> اسم ' + myArray[numWinners + 1])
                 $('#again').fadeIn();
                 title.fadeOut();
-                time = 5000;
+                time = shuffleTime.val() * 1000;
                 numWinners++;
                 $('#again').one('click', function () {
                     $('#again').fadeOut();
                     title.fadeIn();
                     title.text(' اسم ' + myArray[numWinners])
                     startTime();
+                    countdownTimer();
                 });
             }
         }, 2000);
     }
 
+    //show winener in winners container
     function createWinnerContent() {
         let name = $(randomItem).find(".name");
         let phone = $(randomItem).find(".phone");
@@ -171,11 +237,13 @@ $(document).ready(function () {
         $('<div class="information"><p class="p title-win">' + myArray[numWinners] + '</p>' + awardTitle + '<p class="p name">'
             + name.text() + '</p><p class="p phone">' + phone.text() + '</p>').appendTo(winners);
         let id = $(randomItem).attr('id');
-        let test = $('#' + id);
-        $(test).attr('data-id', 1);
-        $(test).css('display', 'none');
+        let target = $('#' + id);
+        // $(target).attr('data-id', 1);
+        // $(target).css('display', 'none');
+        removeRepeatNames(name.text(), phone.text());
     }
 
+    //load file 
     let selectedFile;
     document.getElementById('file-input').addEventListener("change", (event) => {
         selectedFile = event.target.files[0];
@@ -198,7 +266,7 @@ $(document).ready(function () {
             uploaderr.css('display', 'block')
             uploaderr.text('يجب اختيار الملف اولا')
         } else {
-            $('#start').css('display', 'flex')
+            $('#start').css('display', 'block')
             XLSX.utils.json_to_sheet(data, 'out.xlsx');
             if (selectedFile) {
                 let fileReader = new FileReader();
@@ -224,5 +292,28 @@ $(document).ready(function () {
                 + rowObject[i].number + '</p>').appendTo(namesCont);
         }
         $(checkNames).val('names is insert')
+        // test();
     }
+
+    function removeRepeatNames(name, phone) {
+        let names = $('#names .information .name');
+        let phones = $('#names .information .phone');
+        let information = $('#names .information');
+
+        console.log(names);
+        console.log(phones);
+        for (let i = 0; i < names.length; i++) {
+            // let name = $(names[i]).text();
+            // let phone = $(phones[i]).text();
+            console.log(names[i]);
+            // for (let j = i + 1; j < names.length; j++) {
+            if ($(names[i]).text() == name && $(phones[i]).text() == phone) {
+                // $(names[j]).parent().remove();
+                $(information[i]).attr('data-id', 1);
+                $(information[i]).css('display', 'none');
+            }
+            // }
+        }
+    }
+
 });
